0e0a09df65ff673e52bac8e05e7af585ba8b2fde
Mitja Busch <Mitja.Busch@wisetechglobal.com>
2023-10-12 16:13:26 +0200

    Add comments to Deck

--- Deck.cs
+++ Deck.cs
@@ -21,7 +21,11 @@ namespace SnapCall
 			this.removedCards = removedCards;
 			random = new Random();
 			cards = new ulong[52];
+
+			// Generate cards as uInt64 1, 10, 100, ...
 			for (int i = 0; i < 52; i++) cards[i] = 1ul << i;
+
+			// There are no card objects in the deck, instead card draw is simulated by "position", an int used as index for the "cards" ulong[]
 			position = 0;
 		}
 
@@ -32,10 +36,14 @@ namespace SnapCall
 			{
 				n--;
 				int k = random.Next(n + 1);
+
+				// Swap card values at index n and k
 				ulong value = cards[k];
 				cards[k] = cards[n];
 				cards[n] = value;
 			}
+
+			// Reset position to top card, thus shuffle "refills" the deck
 			position = 0;
 		}
 
@@ -44,7 +52,10 @@ namespace SnapCall
 			ulong hand = 0;
 			for (int i = 0; i < count; i++)
 			{
+				// Skip cards that were manually removed via removedCards
 				while ((cards[position] & removedCards) != 0) position++;
+
+				// Add cards to bitmap hand through logical OR (result is 1 if either bit is 1) compound assignment
 				hand |= cards[position];
 				position++;
 			}
